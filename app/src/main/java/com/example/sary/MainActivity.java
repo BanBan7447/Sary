4b759533fa0f7f561d3ca9fbde278c577c555067
thanhphong <pn93948848@gmail.com>
2024-05-13 15:52:35 +0700

    Mới thêm dòng comment bảng 2 nha

--- app/src/main/java/com/example/sary/MainActivity.java
+++ app/src/main/java/com/example/sary/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bảng 2
     }
 }
