c390184a61c999115aa2032cad13d6bb619ba186
Sallysayhiii <kate1225kate@gmail.com>
2024-05-14 13:24:11 +0700

    Mới thêm 2 dòng comment

--- app/src/main/java/com/example/sary/MainActivity.java
+++ app/src/main/java/com/example/sary/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Bảng 2
+        // Bảng giáng sinh
+        // djkgkdjkfgkrnj
     }
 }
