818f245a4520316ec607e40a884602e93ebbf52d
thanhphong <pn93948848@gmail.com>
2024-05-19 20:41:27 +0700

    Em vừa mới tạo giao diện của Trang Intro Trang Đăng nhập Dialog của đăng nhập Danh sách của phiếu mượn
    Và em tạo thêm rất nhiều file layout nữa á
    Cộng thêm là làm tính năng tự động chuyển trang từ intro sang đăng nhập nữa á

--- app/src/main/java/com/example/sary/Page_Home.java
+++ app/src/main/java/com/example/sary/Page_Home.java
@@ -10,5 +10,6 @@ public class Page_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_page_home);
+
     }
 }
